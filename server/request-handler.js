82f72a682e869c4fa4b16b8a9492a4469b869bee
Hack Reactor Students <accounts@hackreactor.com>
2015-09-07 19:35:59 -0700

    implement GET and POST requests, all tests pass

--- server/request-handler.js
+++ server/request-handler.js
@@ -26,12 +26,19 @@ var requestHandler = function(request, response) {
   //
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
+
   // console.logs in your code.
+  // request.url = "https://api.parse.com/1/classes/chatterbox/";
+
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
 
+  //https://api.parse.com/1/classes/chatterbox/ <- note to self: current chatterbox server
+  var statusCode = 200;
+  // The outgoing status.
+  //var messages = [];
+
+  // console.log(request);
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
@@ -39,11 +46,47 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "JSON";
+
+  if (request.method === "GET"){
+    var sendBackToClient;
+    if (messages.hasOwnProperty(request.url)){
+      statusCode = 200;
+      sendBackToClient = JSON.stringify({"results":messages[request.url]});
+    } else {
+      statusCode = 404;
+    }
+      response.writeHead(statusCode, headers);
+      response.end(sendBackToClient);
+  } else if (request.method === "POST") {
+    if (!messages.hasOwnProperty(request.url)){
+      messages[request.url] = [];
+    }
+    statusCode = 201;
+    var message = "";
+    request.on('data', function(data){
+      message += data.toString();
+    });
+    request.on('end', function(){
+      // console.log(messages);
+      message = JSON.parse(message);
+      messages[request.url].push(message);
+      // console.log(messages);
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({"results":messages[request.url]}));
+    });
+    
+  } else if (request.method === "PUT") {
+    statusCode = 201 } else if (request.method === "DELETE") {
+    statusCode = 201;
+  } else if (request.method === "OPTIONS") {
+    statusCode = 201;
+  }
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +95,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+
+  //will store objects that are messages (each one)
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,6 +108,14 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
+
+var messages = {
+  '/classes/room1': [],
+  '/classes/room': [], 
+  '/classes/messages' :[],
+  'http://127.0.0.1:3000/classes/messages': []
+};
+
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -71,3 +123,4 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
+exports.requestHandler = requestHandler;
